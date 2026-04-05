19c6026f66cd2f804d29ebb4cf15a26040123e66
Decpalm <declanpalmateer@gmail.com>
2026-04-05 16:43:11 -0400

    Update programCounter.v

--- programCounter.v
+++ programCounter.v
@@ -6,10 +6,10 @@ module programCounter(
 );
 	always *(posedge clk) begin
 		if(reset) begin
-			pc <= 0;
+			pc = 0;
 		end
 		else begin
-			pc <= pc_next;
+			pc = pc_next;
 		end
 	end
 endmodule
