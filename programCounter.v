ff7605f711df60854d214002742a7ec89d0a0823
Decpalm <declanpalmateer@gmail.com>
2026-04-05 13:01:18 -0400

    Added programCounter.v

---
+++ programCounter.v
@@ -0,0 +1,15 @@
+module programCounter(
+	input wire clk, 
+	imput wire reset, 
+	input wire [31:0] pc_next, 
+	output reg [31:0] pc
+);
+	always *(posedge clk) begin
+		if(reset) begin
+			pc <= 0;
+		end
+		else begin
+			pc <= pc_next;
+		end
+	end
+endmodule
